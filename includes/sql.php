e50b9ba8ee33b0b6886f5af8414f3d526bc45945
Raphael Goncalves <raphaelricardo10@gmail.com>
2021-08-23 12:00:29 -0300

    full

--- includes/sql.php
+++ includes/sql.php
@@ -2,10 +2,6 @@
 
 include './includes/auxiliar.php';
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 function sql_connect()
 {
     include './includes/sqlCredentials.php';
