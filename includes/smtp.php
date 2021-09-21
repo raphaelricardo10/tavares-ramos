610aa83986a982d713379f06d6434a2509d9199e
Raphael <raphaelricardo10@gmail.com>
2021-09-21 00:20:39 -0300

    Added SMTP exception handling

--- includes/smtp.php
+++ includes/smtp.php
@@ -19,7 +19,7 @@ function get_include_contents($filename, $variablesToMakeLocal)
 }
 
 
-function startMail($debug = 0)
+function initMail($debug = 0)
 {
     include './includes/smtpCredentials.php';
     $mail = new PHPMailer;
@@ -38,26 +38,30 @@ function startMail($debug = 0)
 
 function sendMailToServer(array $postData, $mailSubject, String $fileHTML, int $debug = 0)
 {
-    $mail = startMail($debug);
-    $mail->addReplyTo($postData['email'], sprintf('%s %s', $postData['name'], $postData['surname']));
-    $mail->addAddress('contato@tavaresramos.com.br', 'Contato via Site');
-    $mail->Subject = $mailSubject;
-    $mail->Body = get_include_contents($fileHTML, $postData);
-    $mail->IsHTML(true);
-    if (!$mail->send()) {
-        return false;
-    } else {
-        return true;
+    try {
+        $mail = initMail($debug);
+        $mail->addReplyTo($postData['email'], sprintf('%s %s', $postData['name'], $postData['surname']));
+        $mail->addAddress('contato@tavaresramos.com.br', 'Contato via Site');
+        $mail->Subject = $mailSubject;
+        $mail->Body = get_include_contents($fileHTML, $postData);
+        $mail->IsHTML(true);
+        $mail->send();
+    } catch (Exception $e) {
+        throw $e;
     }
 }
 
 function sendMailToClient(array $postData, String $fileHTML, int $debug = 0)
 {
-    $mail = startMail($debug);
-    $mail->addReplyTo('contato@tavaresramos.com.br', 'Tavares Ramos');
-    $mail->addAddress($postData['email'], sprintf('%s %s', $postData['name'], $postData['surname']));
-    $mail->Subject = 'Tavares Ramos - Recebemos seu contato';
-    $mail->Body = get_include_contents($fileHTML, $postData);
-    $mail->IsHTML(true);
-    $mail->send();
+    try {
+        $mail = initMail($debug);
+        $mail->addReplyTo('contato@tavaresramos.com.br', 'Tavares Ramos');
+        $mail->addAddress($postData['email'], sprintf('%s %s', $postData['name'], $postData['surname']));
+        $mail->Subject = 'Tavares Ramos - Recebemos seu contato';
+        $mail->Body = get_include_contents($fileHTML, $postData);
+        $mail->IsHTML(true);
+        $mail->send();
+    } catch (Exception $e) {
+        throw $e;
+    }
 }
